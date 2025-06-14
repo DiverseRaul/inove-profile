28f8dc0eb05b05f425ad64a02ad2b7e786a13483
DiverseRaul <goraulseixas@gmail.com>
2025-06-13 21:18:53 -0400

    github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // For custom domain on GitHub Pages
   plugins: [vue()],
   resolve: {
     alias: {
